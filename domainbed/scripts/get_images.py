e453cb5de89aca3f87c1bf229a22f5d0b60c70dd
Yuxiang Wang <963323759@qq.com>
2022-10-08 09:12:38 +0800

    fix bug of image generation

--- domainbed/scripts/get_images.py
+++ domainbed/scripts/get_images.py
@@ -1,4 +1,5 @@
 import h5py
+import sys
 from pathlib import Path
 from scipy import signal
 from scipy.interpolate import interp2d
@@ -6,6 +7,7 @@ from matplotlib import pyplot as plt
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 if __name__ == "__main__":
     data_segment = 5120 // 5
@@ -28,34 +30,37 @@ if __name__ == "__main__":
     files = [f for f in path_orig.glob('*') if not f.is_dir()]
     fault_types = ['正常', '裂纹', '断齿', '缺齿', '磨损', '点蚀', '磨损+点蚀', '裂纹+缺齿', '裂纹+断齿']
 
-    for file in files:
-        name = file.name.split(".")[0]
-        folder = path_out / name
-        if not folder.exists():
-            folder.mkdir()
+    input = sys.argv
+    file = files[int(input[1])]
+    name = file.name.split(".")[0]
+    folder = path_out / name
+    if not folder.exists():
+        folder.mkdir()
 
-        read_file = h5py.File(file, 'r')
-        signals = pd.DataFrame(np.array(read_file['x']))
-        read_file.close()
+    read_file = h5py.File(file, 'r')
+    signals = pd.DataFrame(np.array(read_file['x']))
+    read_file.close()
 
-        for index, fault_type in enumerate(fault_types):
-            fault_type_folder = folder / fault_type
-            if not fault_type_folder.exists():
-                fault_type_folder.mkdir()
+    for index, fault_type in enumerate(fault_types):
+        fault_type_folder = folder / fault_type
+        if not fault_type_folder.exists():
+            fault_type_folder.mkdir()
 
-            for beg in range(0, signals.shape[1] - data_segment, data_segment):
-                sig = signals.iloc[index][beg: beg + data_segment]
+        for beg in tqdm(range(0, signals.shape[1] - data_segment, data_segment)):
+            sig = signals.iloc[index][beg: beg + data_segment]
 
-                if max(sig) < 0.01:
-                    continue
+            if max(sig) < 0.01:
+                continue
 
-                f, t, Zxx = signal.stft(sig, sampling_frequency, window=window, nperseg=nperseg, noverlap=noverlap)
-                newF = interp2d(t, f, np.abs(Zxx), kind='linear')
-                t_new = np.linspace(0, 1 / sampling_frequency * data_segment, num=x_len)
-                f_new = np.linspace(0, sampling_frequency // 2, num=f_len)
-                Z_new = newF(t_new, f_new)
-                Xn, Yn = np.meshgrid(t_new, f_new)
+            f, t, Zxx = signal.stft(sig, sampling_frequency, window=window, nperseg=nperseg, noverlap=noverlap)
+            newF = interp2d(t, f, np.abs(Zxx), kind='linear')
+            t_new = np.linspace(0, 1 / sampling_frequency * data_segment, num=x_len)
+            f_new = np.linspace(0, sampling_frequency // 2, num=f_len)
+            Z_new = newF(t_new, f_new)
+            Xn, Yn = np.meshgrid(t_new, f_new)
 
-                plt.pcolormesh(Xn, Yn, Z_new, shading='auto')
-                plt.axis('off')
-                plt.savefig(fault_type_folder / (str(beg) + '.png'), dpi=300, bbox_inches='tight', pad_inches=0)
+            plt.pcolormesh(Xn, Yn, Z_new, shading='auto')
+            plt.axis('off')
+            # plt.savefig(fault_type_folder / (str(beg) + '.png'), dpi=300, bbox_inches='tight', pad_inches=0)
+            plt.clf()
+            plt.close()
